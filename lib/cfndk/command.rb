a0135e7d727c7b2b08f01f08001e3357c1c816c8
Yoshihisa AMAKATA <amakata@flatz.jp>
2019-03-08 21:16:17 +0900

    refactoring of config file load

--- lib/cfndk/command.rb
+++ lib/cfndk/command.rb
@@ -182,15 +182,7 @@ module CFnDK
     option :keypair_names, type: :array
     def create
       CFnDK.logger.info 'create...'.color(:green)
-      unless File.file?(options[:config_path])
-        CFnDK.logger.error "File does not exist. #{options[:config_path]}".color(:red)
-        return 1
-      end
-      data = open(options[:config_path], 'r') { |f| YAML.load(f) }
-      unless data
-        CFnDK.logger.error "File is empty. #{options[:config_path]}".color(:red)
-        return 1
-      end
+      data = load_config_data(options)
 
       credentials = CFnDK::CredentialProviderChain.new.resolve
       stacks = CFnDK::Stacks.new(data, options, credentials)
@@ -214,15 +206,7 @@ module CFnDK
     option :keypair_names, type: :array
     def destroy
       CFnDK.logger.info 'destroy...'.color(:green)
-      unless File.file?(options[:config_path])
-        CFnDK.logger.error "File does not exist. #{options[:config_path]}".color(:red)
-        return 1
-      end
-      data = open(options[:config_path], 'r') { |f| YAML.load(f) }
-      unless data
-        CFnDK.logger.error "File is empty. #{options[:config_path]}".color(:red)
-        return 1
-      end
+      data = load_config_data(options)
 
       credentials = CFnDK::CredentialProviderChain.new.resolve
       stacks = CFnDK::Stacks.new(data, options, credentials)
@@ -249,15 +233,7 @@ module CFnDK
     def report
       CFnDK.logger.info 'report...'.color(:green)
 
-      unless File.file?(options[:config_path])
-        CFnDK.logger.error "File does not exist. #{options[:config_path]}".color(:red)
-        return 1
-      end
-      data = open(options[:config_path], 'r') { |f| YAML.load(f) }
-      unless data
-        CFnDK.logger.error "File is empty. #{options[:config_path]}".color(:red)
-        return 1
-      end
+      data = load_config_data(options)
 
       credentials = CFnDK::CredentialProviderChain.new.resolve
       stacks = CFnDK::Stacks.new(data, options, credentials)
@@ -281,5 +257,15 @@ module CFnDK
     subcommand 'keypair', KeyPairCommand
     desc 'stack SUBCOMMAND ...ARGS', 'manage stack'
     subcommand 'stack', StackCommand
+
+    private
+
+    def load_config_data(options)
+      raise "File does not exist. #{options[:config_path]}" unless File.file?(options[:config_path])
+      data = open(options[:config_path], 'r') { |f| YAML.load(f) }
+      return data if data
+      CFnDK.logger.error "File is empty. #{options[:config_path]}".color(:red)
+      nil
+    end
   end
 end
