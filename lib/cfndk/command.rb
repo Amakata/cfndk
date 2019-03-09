e10d7643a6ab6cbf3e19a792b3a36d79e678eea8
Yoshihisa AMAKATA <amakata@flatz.jp>
2019-03-09 14:24:48 +0900

    support subcommand help exit code and refactoring

--- lib/cfndk/command.rb
+++ lib/cfndk/command.rb
@@ -1,12 +1,41 @@
 module CFnDK
+  module SubcommandHelpReternable
+    module ClassMethods
+      def subcommand_help(cmd)
+        desc 'help [COMMAND]', 'Describe subcommands or one specific subcommand'
+        class_eval "
+          def help(command = nil, subcommand = true); super; return 2; end
+  "
+      end
+    end
+    extend ClassMethods
+    def self.included(klass)
+      klass.extend ClassMethods
+    end
+  end
+
+  module ConfigFileLoadable
+    private
+
+    def load_config_data(options)
+      raise "File does not exist. #{options[:config_path]}" unless File.file?(options[:config_path])
+      data = open(options[:config_path], 'r') { |f| YAML.load(f) }
+      return data if data
+      CFnDK.logger.error "File is empty. #{options[:config_path]}".color(:red)
+      nil
+    end
+  end
+
   class KeyPairCommand < Thor
+    include SubcommandHelpReternable
+    include ConfigFileLoadable
     class_option :verbose, type: :boolean, aliases: 'v'
     class_option :color, type: :boolean, default: true
     class_option :config_path, type: :string, aliases: 'c', default: "#{Dir.getwd}/cfndk.yml"
     class_option :uuid, type: :string, aliases: 'u', default: ENV['CFNDK_UUID'] || nil
     class_option :keypair_names, type: :array
 
-    desc 'create', 'create keypair'
+    desc 'create', 'Create keypair'
     option :properties, type: :hash, aliases: 'p', default: {}
     def create
       CFnDK.logger.info 'create...'.color(:green)
@@ -22,7 +51,7 @@ module CFnDK
       return 1
     end
 
-    desc 'destroy', 'destroy keypair'
+    desc 'destroy', 'Destroy keypair'
     option :force, type: :boolean, aliases: 'f', default: false
     def destroy
       CFnDK.logger.info 'destroy...'.color(:green)
@@ -43,26 +72,18 @@ module CFnDK
       CFnDK.logger.debug e.backtrace
       return 1
     end
-
-    private
-
-    def load_config_data(options)
-      raise "File does not exist. #{options[:config_path]}" unless File.file?(options[:config_path])
-      data = open(options[:config_path], 'r') { |f| YAML.load(f) }
-      return data if data
-      CFnDK.logger.error "File is empty. #{options[:config_path]}".color(:red)
-      nil
-    end
   end
 
   class StackCommand < Thor
+    include SubcommandHelpReternable
+    include ConfigFileLoadable
+
     class_option :verbose, type: :boolean, aliases: 'v'
     class_option :color, type: :boolean, default: true
     class_option :config_path, type: :string, aliases: 'c', default: "#{Dir.getwd}/cfndk.yml"
     class_option :stack_names, type: :array
 
-
-    desc 'create', 'create stack'
+    desc 'create', 'Create stack'
     option :uuid, type: :string, aliases: 'u', default: ENV['CFNDK_UUID'] || nil
     option :properties, type: :hash, aliases: 'p', default: {}
     def create
@@ -80,7 +101,7 @@ module CFnDK
       return 1
     end
 
-    desc 'update', 'update stack'
+    desc 'update', 'Update stack'
     option :uuid, type: :string, aliases: 'u', default: ENV['CFNDK_UUID'] || nil
     option :properties, type: :hash, aliases: 'p', default: {}
     def update
@@ -98,7 +119,7 @@ module CFnDK
       return 1
     end
 
-    desc 'destroy', 'destroy stack'
+    desc 'destroy', 'Destroy stack'
     option :force, type: :boolean, aliases: 'f', default: false
     option :uuid, type: :string, aliases: 'u', default: ENV['CFNDK_UUID'] || nil
     def destroy
@@ -121,7 +142,7 @@ module CFnDK
       return 1
     end
 
-    desc 'validate', 'validate stack'
+    desc 'validate', 'Validate stack'
     def validate
       CFnDK.logger.info 'validate...'.color(:green)
       data = load_config_data(options)
@@ -135,7 +156,7 @@ module CFnDK
       return 1
     end
 
-    desc 'report', 'report stack'
+    desc 'report', 'Report stack'
     option :uuid, type: :string, aliases: 'u', default: ENV['CFNDK_UUID'] || nil
     def report
       CFnDK.logger.info 'report...'.color(:green)
@@ -149,20 +170,11 @@ module CFnDK
       CFnDK.logger.debug e.backtrace
       return 1
     end
-
-    private
-
-    def load_config_data(options)
-      raise "File does not exist. #{options[:config_path]}" unless File.file?(options[:config_path])
-      data = open(options[:config_path], 'r') { |f| YAML.load(f) }
-      return data if data
-      CFnDK.logger.error "File is empty. #{options[:config_path]}".color(:red)
-      nil
-    end
   end
 
   class Command < Thor
     include Thor::Actions
+    include ConfigFileLoadable
     class << self
       def exit_on_failure?
         true
@@ -177,19 +189,19 @@ module CFnDK
     class_option :verbose, type: :boolean, aliases: 'v'
     class_option :color, type: :boolean, default: true
 
-    desc 'generate-uuid', 'print UUID'
+    desc 'generate-uuid', 'Print UUID'
     def generate_uuid
       puts SecureRandom.uuid
       0
     end
 
-    desc 'version', 'print version'
+    desc 'version', 'Print version'
     def version
       puts CFnDK::VERSION
       0
     end
 
-    desc 'init', 'craete sample cfndk.yml & CloudFormation yaml & json files.'
+    desc 'init', 'Craete sample cfndk.yml & CloudFormation yaml & json files.'
     def init
       config_path = "#{Dir.getwd}/cfndk.yml"
       if File.file?(config_path)
@@ -201,7 +213,7 @@ module CFnDK
       CFnDK.logger.info "create #{config_path}".color(:green)
     end
 
-    desc 'create', 'create keypair & stack'
+    desc 'create', 'Create keypair & stack'
     option :config_path, type: :string, aliases: 'c', default: "#{Dir.getwd}/cfndk.yml"
     option :uuid, type: :string, aliases: 'u', default: ENV['CFNDK_UUID'] || nil
     option :properties, type: :hash, aliases: 'p', default: {}
@@ -225,7 +237,7 @@ module CFnDK
       return 1
     end
 
-    desc 'destroy', 'destroy keypair & stack'
+    desc 'destroy', 'Destroy keypair & stack'
     option :config_path, type: :string, aliases: 'c', default: "#{Dir.getwd}/cfndk.yml"
     option :force, type: :boolean, aliases: 'f', default: false
     option :uuid, type: :string, aliases: 'u', default: ENV['CFNDK_UUID'] || nil
@@ -253,7 +265,7 @@ module CFnDK
       return 1
     end
 
-    desc 'report', 'report stack'
+    desc 'report', 'Report stack'
     option :config_path, type: :string, aliases: 'c', default: "#{Dir.getwd}/cfndk.yml"
     option :uuid, type: :string, aliases: 'u', default: ENV['CFNDK_UUID'] || nil
     option :stack_names, type: :array, aliases: 'n'
@@ -280,19 +292,9 @@ module CFnDK
       end
     end
 
-    desc 'keypair SUBCOMMAND ...ARGS', 'manage keypair'
+    desc 'keypair SUBCOMMAND ...ARGS', 'Manage keypair'
     subcommand 'keypair', KeyPairCommand
-    desc 'stack SUBCOMMAND ...ARGS', 'manage stack'
+    desc 'stack SUBCOMMAND ...ARGS', 'Manage stack'
     subcommand 'stack', StackCommand
-
-    private
-
-    def load_config_data(options)
-      raise "File does not exist. #{options[:config_path]}" unless File.file?(options[:config_path])
-      data = open(options[:config_path], 'r') { |f| YAML.load(f) }
-      return data if data
-      CFnDK.logger.error "File is empty. #{options[:config_path]}".color(:red)
-      nil
-    end
   end
 end
